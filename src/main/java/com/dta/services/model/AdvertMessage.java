b0fd636f850a10f27211a0d21e036d9e9b0ba156
Jonathan Couroyer <taerus.dev@outlook.fr>
2015-09-15 17:30:44 +0200

    entity implementation:   - Message.java   - AdvertMessage.java   - PrivateMessage.java

--- src/main/java/com/dta/services/model/AdvertMessage.java
+++ src/main/java/com/dta/services/model/AdvertMessage.java
@@ -1,9 +1,32 @@
 package com.dta.services.model;
 
+import javax.persistence.Entity;
+import java.util.Date;
 
+@Entity
 public class AdvertMessage extends Message {
+
+    private String title;
 
     public AdvertMessage() {
     }
 
+
+    // --------- constructors ----------
+
+    public AdvertMessage(Date creationDate, String content, User author, String title) {
+        super(creationDate, content, author);
+        this.title = title;
+    }
+
+
+    // -------- getter / setter --------
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
