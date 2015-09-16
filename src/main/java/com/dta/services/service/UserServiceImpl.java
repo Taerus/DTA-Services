e771454d049d0fbf32479b206016786bb94b3fc4
nubinub <fab_thomas@msn.com>
2015-09-16 14:51:57 +0200

    User service and tests

---
+++ src/main/java/com/dta/services/service/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package com.dta.services.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dta.services.dao.IUserDao;
+import com.dta.services.model.User;
+
+@Service("userService")
+public class UserServiceImpl implements IUserService {
+	@Autowired
+	private IUserDao userDao;
+
+	@Transactional(rollbackFor=Exception.class)
+	public void createUser(User user) {
+		userDao.create(user);		
+	}
+
+	@Transactional(readOnly=true)
+	public List<User> getAll() {
+		return userDao.list();
+	}
+
+}
