0be9d8834ae88261b98376a7ed206bbe6eb010f6
Jonathan Couroyer <taerus.dev@outlook.fr>
2015-09-18 14:51:20 +0200

    feature: send private message

--- src/main/java/com/dta/services/controller/HomeController.java
+++ src/main/java/com/dta/services/controller/HomeController.java
@@ -2,10 +2,12 @@ package com.dta.services.controller;
 
 import javax.validation.Valid;
 
-import com.dta.services.model.AdvertType;
+import com.dta.services.model.*;
 import com.dta.services.service.IAdvertService;
+import com.dta.services.service.IMessageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import com.dta.services.model.Advert;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -13,8 +15,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.dta.services.model.Role;
-import com.dta.services.model.User;
 import com.dta.services.service.IUserService;
 import com.dta.services.utils.PasswordEncoder;
 
@@ -30,6 +30,9 @@ public class HomeController {
 
 	@Autowired
 	private IAdvertService advertService;
+
+	@Autowired
+	private IMessageService messageService;
 
 	@Autowired
 	private PasswordEncoder passwordEncoder;
@@ -92,6 +95,37 @@ public class HomeController {
 		advertService.createAdvert(advert);
 
         return "redirect:/";
+    }
+
+	@RequestMapping(value = "message/new", method = RequestMethod.GET)
+	public String writeMessage(Model model) {
+        PrivateMessage message = new PrivateMessage();
+
+        model.addAttribute("message", message);
+
+		return "NewPrivateMessage";
+	}
+
+    @RequestMapping(value = "message/send", method = RequestMethod.POST)
+    public String sendMessage(@Valid PrivateMessage message, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "NewPrivateMessage";
+        }
+
+        message.setAuthor(getLoggedUser());
+        message.setCreationDate(new Date());
+        messageService.postMessage(message);
+
+        return "Home";
+    }
+
+    private User getLoggedUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        Object user = (auth != null) ? auth.getPrincipal() :  null;
+
+        return (user instanceof User) ? (User)user : null;
     }
 
 }
---
+++ src/main/java/com/dta/services/service/MessageServiceImpl.java
@@ -0,0 +1,81 @@
+package com.dta.services.service;
+
+import com.dta.services.dao.IMessageDao;
+import com.dta.services.model.AdvertMessage;
+import com.dta.services.model.Message;
+import com.dta.services.model.PrivateMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class MessageServiceImpl implements IMessageService {
+
+    @Autowired
+    IMessageDao messageDao;
+
+//    @Autowired
+//    IAdvertMessageDao advertMessageDao;
+//
+//    @Autowired
+//    IPrivateMessageDao privateMessageDao;
+
+
+    // ---------------- Messages ----------------
+
+    @Override
+    public void postMessage(Message message) {
+        messageDao.create(message);
+    }
+
+    @Override
+    public void deleteMessage(long id) {
+        messageDao.delete(id);
+    }
+
+
+    // ------------ Private messages ------------
+
+    @Override
+    @Transactional(readOnly = true)
+    public PrivateMessage getPrivateMessage(long id) {
+        return null;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<PrivateMessage> listSentPrivateMessages(long senderId) {
+        return null;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<PrivateMessage> listReceivedPrivateMessages(long receiverId) {
+        return null;
+    }
+
+
+    // ------------ Advert messages ------------
+
+    @Override
+    @Transactional(readOnly = true)
+    public AdvertMessage getAdvertMessage(long id) {
+        return null;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<AdvertMessage> listSentAdvertMessages(long senderId) {
+        return null;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<AdvertMessage> listReceivedAdvertMessages(long receiverId) {
+        return null;
+    }
+    
+}
