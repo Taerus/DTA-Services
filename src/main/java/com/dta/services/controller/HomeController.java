ea28167626352d7891384326865da8ffa5adc67c
nubinub <fab_thomas@msn.com>
2015-09-24 10:02:30 +0200

    Fix register security

--- src/main/java/com/dta/services/controller/HomeController.java
+++ src/main/java/com/dta/services/controller/HomeController.java
@@ -46,7 +46,6 @@ public class HomeController {
 		return new User();
 	}
 
-	@Secured("isAnonymous")
 	@RequestMapping(value="register",method=RequestMethod.POST)
 	public String registerUser(@Valid User user,BindingResult bindingResult, Model model){
 		
